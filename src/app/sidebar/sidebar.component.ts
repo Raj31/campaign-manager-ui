89f14cc56119fec362742e23bb17543a21059e37
Prashant Yadav <prashantyadav@Prashants-MacBook-Air.local>
2018-07-02 13:57:42 -0400

    campaign changes + Sidebar changes

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,14 +8,15 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: 'dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+//    { path: 'dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+//    { path: 'advertisers', title: 'Advertisers',  icon:'pe-7s-id', class: '' },
     { path: 'user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
     { path: 'campaigns', title: 'Campaigns',  icon:'pe-7s-note2', class: '' },
-    { path: 'typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
-    { path: 'icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
-    { path: 'maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
-    { path: 'notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
-    { path: 'upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
+//    { path: 'typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
+//    { path: 'icons', title: 'Icons',  icon:'pe-7s-science', class: '' }
+//    { path: 'maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
+//    { path: 'notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
+//    { path: 'upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
 ];
 
 @Component({
